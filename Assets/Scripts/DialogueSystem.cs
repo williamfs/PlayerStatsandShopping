8428c05e973e148b5c69669db449456bcf03fbf3
williamfs <CSheu@student.fullsail.edu>
2020-06-20 05:42:55 -0400

    Player movement & Dialogue system setup

---
+++ Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+    public string[] storyLine;
+
+    public GameObject dialogue;
+    public Text dialogueText;
+    public Text nameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ShowDialogue(string[] lines)
+    {
+        dialogue.SetActive(true);
+        storyLine = lines;
+        dialogueText.text = storyLine[0];
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Dialogue")
+        {
+            ShowDialogue(collision.transform.parent.GetComponent<NPC>().storyLine);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float xMove;
+    private float yMove;
+    public float MoveSpeed;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMove = Input.GetAxisRaw("Horizontal");
+        yMove = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector2(xMove * MoveSpeed, yMove * MoveSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
