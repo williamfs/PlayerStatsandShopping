bfde6193cfc3d4ccb63c5d00138b9295528a280a
williamfs <CSheu@student.fullsail.edu>
2020-06-21 11:46:55 -0400

    Dialogue system finish
    Player will stop when talking and getting story line from the right NPC

--- Assets/Scripts/DialogueSystem.cs
+++ Assets/Scripts/DialogueSystem.cs
@@ -6,35 +6,80 @@ using UnityEngine.UI;
 public class DialogueSystem : MonoBehaviour
 {
     public string[] storyLine;
+    private int currentIndex;
+    public bool canTalk;
+    private bool hasStarted;
+    public NPC activeNPC;
 
     public GameObject dialogue;
     public Text dialogueText;
     public Text nameText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (canTalk && Input.GetMouseButtonDown(0))
+        {
+            if (!hasStarted)
+            {
+                hasStarted = true;
+                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                GetComponent<PlayerControl>().canMove = false;
+                ShowDialogue(activeNPC.storyLine);
+            }
+            else
+            {
+                ProceedNext();
+            }
+        }
     }
 
     private void ShowDialogue(string[] lines)
     {
         dialogue.SetActive(true);
         storyLine = lines;
-        dialogueText.text = storyLine[0];
+        currentIndex = 0;
+        CheckIfName(currentIndex);
+        dialogueText.text = storyLine[currentIndex];
+    }
+
+    private void ProceedNext()
+    {
+        currentIndex++;
+        if (currentIndex < storyLine.Length)
+        {
+            CheckIfName(currentIndex);
+            dialogueText.text = storyLine[currentIndex];
+        }
+        else
+        {
+            currentIndex = 0;
+            hasStarted = false;
+            GetComponent<PlayerControl>().canMove = true;
+            dialogue.SetActive(false);
+        }
+    }
+
+    private void CheckIfName(int i)
+    {
+        if (storyLine[i].StartsWith("N_"))
+        {
+            nameText.text = storyLine[i].Replace("N_", "");
+            currentIndex++;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Dialogue")
         {
-            ShowDialogue(collision.transform.parent.GetComponent<NPC>().storyLine);
+            canTalk = true;
+            activeNPC = collision.transform.parent.GetComponent<NPC>();
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canTalk = false;
     }
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,26 +7,31 @@ public class PlayerControl : MonoBehaviour
     private float xMove;
     private float yMove;
     public float MoveSpeed;
+    public bool canMove;
 
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        canMove = true;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        xMove = Input.GetAxisRaw("Horizontal");
-        yMove = Input.GetAxisRaw("Vertical");
+        if (canMove)
+        {
+            xMove = Input.GetAxisRaw("Horizontal");
+            yMove = Input.GetAxisRaw("Vertical");
+            rb.velocity = new Vector2(xMove * MoveSpeed, yMove * MoveSpeed);
+        }
 
-        rb.velocity = new Vector2(xMove * MoveSpeed, yMove * MoveSpeed);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+
     }
 }
